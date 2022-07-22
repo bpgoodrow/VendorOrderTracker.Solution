3c8e39875292e6f60a233acc810527cccff36b93
Ben Goodrow <bpgoodrow@gmail.com>
2022-07-22 16:06:33 -0700

    Vendor test 1 successful

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -1,25 +1,24 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using VendorOrder.Models;
-// using System.Collections.Generic;
-// using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrder.Models;
+using System.Collections.Generic;
+using System;
 
-// namespace Vendor.Tests
-// {
-//   [TestClass]
-//   public class VendorTests : IDisposable
-//   {
-//     public void Dispose()
-//     {
-//       Vendor.ClearAll();
-//     }
+namespace VendorOrder.Tests
+{
+  [TestClass]
+    public class VendorTests : IDisposable
+    {
+      public void Dispose()
+      {
+        Vendor.ClearAll();
+      }
 
-//     [TestMethod]
-//     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
-//     {
-//       Vendor newVendor = new Vendor("test vendor");
-//       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
-//     }
-
-//     
-//   }
-// }
+      [TestMethod]
+      public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+      {
+      Vendor newVendor = new Vendor("test vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+      }
+    }
+  
+}
