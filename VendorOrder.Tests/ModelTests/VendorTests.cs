17abfad25faad13a4ebdb7e984dfdb7d2407f89a
Ben Goodrow <bpgoodrow@gmail.com>
2022-07-22 16:20:43 -0700

    Vendor class test 4 successful

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -34,5 +34,16 @@ namespace VendorOrder.Tests
       int result = newVendor.Id;
       Assert.AreEqual(1, result);
     }
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_CategoryList()
+    {
+      string name01 = "Joes Crabs";
+      string name02 = "Cajun Coffee";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
