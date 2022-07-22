b8419956e320a8ee0c074bd09761954aad8ea9d2
Ben Goodrow <bpgoodrow@gmail.com>
2022-07-22 11:08:10 -0700

    create model and controller for vendor class

---
+++ VendorOrder/Models/Vendor.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+
+namespace VendorOrder.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+
+    public Vendor(string vendorName)
+    {
+      Name = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Records = new List<Record>{};
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
+    }
+  }
+}
