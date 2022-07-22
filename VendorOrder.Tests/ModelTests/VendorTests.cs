1099abda274681020d5510d260df63a9a28ed3e0
Ben Goodrow <bpgoodrow@gmail.com>
2022-07-22 14:42:05 -0700

    fixed bug in packagereference test.sdk -- first Order test successful

---
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,38 @@
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using VendorOrder.Models;
+// using System.Collections.Generic;
+// using System;
+
+// namespace Vendor.Tests
+// {
+//   [TestClass]
+//   public class VendorTests : IDisposable
+//   {
+//     public void Dispose()
+//     {
+//       Vendor.ClearAll();
+//     }
+
+//     [TestMethod]
+//     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+//     {
+//       Vendor newVendor = new Vendor("test vendor");
+//       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+//     }
+
+
+//     [TestMethod]
+//     public void GetName_ReturnsName_String()
+//     {
+//       //Arrange
+//       string name = "Test vendor";
+//       Vendor newVendor = new Vendor(name);
+
+//       //Act
+//       string result = newVendor.Name;
+
+//       //Assert
+//       Assert.AreEqual(name, result);
+//     }
+//   }
+// }
