0ceadfbc4332e35756e3f42161cc730afe70f62d
Ben Goodrow <bpgoodrow@gmail.com>
2022-07-22 16:09:58 -0700

    Vendor class test 2 successful

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -11,14 +11,20 @@ namespace VendorOrder.Tests
       public void Dispose()
       {
         Vendor.ClearAll();
-      }
-
+      } 
       [TestMethod]
       public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
       {
       Vendor newVendor = new Vendor("test vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
       }
+      [TestMethod]
+      public void GetVendorName_ReturnsName_String()
+      {
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
+      string result = newVendor.Name;
+      Assert.AreEqual(name, result);
+      }
     }
-  
 }
